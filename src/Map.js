69c71651f000b755e4f5025baa75deee0bb33941
YoChenYin <shelly7526@gmail.com>
2018-07-29 23:53:20 +0800

    add color to map

--- src/Map.js
+++ src/Map.js
@@ -2,25 +2,92 @@ import React, { Component } from 'react'
 import L from 'leaflet'
 import {feature} from "topojson-client"
 
+const a = {
+    "R01":10,
+    "R02":40,
+    "R03":30,
+    "R04":50,
+    "R05":7,
+    "R06":2,
+    "R07":12,
+    "R08":22,
+    "R09":21,
+    "R10":12,
+    "R11":1,
+    "R12":23,
+    "R17":11,
+    "R13":2,
+    "R18":2,
+    "R19":2,
+    "R20":21,
+    "R21":2,
+    "R22":22,
+    "R23":2,
+    "R24":23,
+    "R25":2,
+    "R26":2,
+    "R27":2,
+    "R28":2,
+    "R29":42,
+    "R30":82,
+    "R31":2,
+    "D04":2,
+    "R14":2,
+    "R15":2,
+    "R16":42,
+    "D06":2,
+    "D02":22,
+    "D01":12,
+    "D07":2,
+    "D08":2,
+}
+
 class Map extends Component{
     constructor(props){
         super(props)
         this.state = {
             poplData:{},
-        }    
+        }   
+        
     }
     
     componentDidMount(){
-        fetch(this.props.data.towngeo)
+        function getColor(d){
+            return d > 100 ? '#FFEDA0':
+                   d > 80  ? '#BD0026':
+                   d > 60  ? '#E31A1C':
+                   d > 40  ? '#FC4E2A':
+                   d > 20  ? '#FD8D3C':
+                   d > 0   ? '#FEB24C':
+                   d === 0 ? '#FED976':
+                            '#FFEDA0';
+        }
+        fetch('./data/tainan.json')
         .then(res => {
             if(res.status !== 200){
                 console.log(`There was a problem: ${res.status}`)
                 return
             }
             res.json().then(topology => {
+                
+                topology.features.map((d)=>{
+                    d.properties.casenum = a[d.properties.TOWNID]
+                })
+                
                 this.setState({
                   poplData:topology.features
-                },()=>L.geoJSON(this.state.poplData).addTo(map)  )
+                },()=>L.geoJSON(this.state.poplData,{
+                    style:function(feature){
+                        return {
+                            fillColor:getColor(feature.properties.casenum),
+                            weight: 2,
+                            opacity: 1,
+                            color: 'white',
+                            fillOpacity: 0.7
+                        }
+                    }
+                
+                }).addTo(map))
             })
         })
         var map = L.map('map',{
