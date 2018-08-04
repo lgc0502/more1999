02c6338da2bbc71c852f9add3da3e1f281c57257
YoChenYin <shelly7526@gmail.com>
2018-08-05 02:38:21 +0800

    create calculate date function

--- src/Date.js
+++ src/Date.js
@@ -1,4 +1,3 @@
-
 function crossmonth(today,cross,date,label){
     let i = 1
     let date31 = [1,3,5,7,8,10,12]
@@ -6,6 +5,7 @@ function crossmonth(today,cross,date,label){
         cross.enddate = 1;
     else
         cross.begindate = 1;
+    
     for (i=0;i<date31.length;i++)
     {
         if (today.getMonth()+1 == date31[i] )
@@ -15,22 +15,25 @@ function crossmonth(today,cross,date,label){
 }
 
 export default {
-    
-    
     lastweekdate:()=>{
         let today = new Date()
         let cross = {begindate:0,enddate:0}
         
-        
         let end_date = today.getDate() - today.getDay()
-        end_date = (end_date>0)?end_date:crossmonth(today,cross,"end")
+        end_date = (end_date>0)?end_date:crossmonth(today,cross,end_date,"end")
+        end_date = (end_date>10)?end_date:"0"+end_date
         
         let begin_date = end_date -7
-        begin_date  = (begin_date>0)?begin_date :crossmonth(today,cross,"begin")
-      
+        begin_date  = (begin_date>0)?begin_date :crossmonth(today,cross,begin_date,"begin")
+        begin_date = (begin_date>10)?begin_date:"0"+begin_date
+
+        let begin_month = (cross.enddate === 0 & cross.begindate === 0)?today.getMonth()+1:today.getMonth()
+        begin_month = (today.getMonth()<10) ? "0"+begin_month:begin_month
+        let end_month = (cross.enddate === 0)?today.getMonth()+1:today.getMonth()
+        end_month = (today.getMonth()<10) ? "0"+end_month:end_month
         //y-m-d
-        begin_date  = (today.getMonth()<10) ? today.getFullYear()+"-"+"0"+(cross.enddate === 0&cross.begindate == 0?today.getMonth()+1:today.getMonth())+"-"+begin_date : today.getFullYear()+"-"+(today.getMonth()+1)+"-"+begin_date;
-        end_date = (today.getMonth()<10) ? today.getFullYear()+"-"+"0"+(cross.enddate === 0?today.getMonth()+1:today.getMonth())+"-"+end_date : today.getFullYear()+"-"+(today.getMonth()+1)+"-"+end_date;
+        begin_date  =today.getFullYear()+"-"+begin_month+"-"+begin_date 
+        end_date = today.getFullYear()+"-"+end_month+"-"+end_date
         
         return {
             begin:begin_date,
