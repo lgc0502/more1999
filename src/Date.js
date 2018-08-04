dcb181af6fa9f56e7417365d079bfca2a17b9eaf
YoChenYin <shelly7526@gmail.com>
2018-08-05 01:16:00 +0800

    create calculate date function

--- src/Date.js
+++ src/Date.js
@@ -1,7 +1,7 @@
-function crossmonth(date,label){
+function crossmonth(today,begindatecross,enddatecross,date,label){
     let i = 1
     let date31 = [1,3,5,7,8,10,12]
-    if (label == "end")
+    if (label === "end")
         enddatecross = 1;
     else
         begindatecross = 1;
@@ -21,14 +21,14 @@ export default {
         let begindatecross = 0
         
         let end_date = today.getDate() - today.getDay()
-        end_date = (end_date>0)?end_date:crossmonth(end_date,"end")
+        end_date = (end_date>0)?end_date:crossmonth(today,begindatecross,enddatecross,end_date,"end")
         
         let begin_date = end_date -7
-        begin_date  = (begin_date>0)?begin_date :crossmonth(begin_date,"begin")
+        begin_date  = (begin_date>0)?begin_date :crossmonth(today,begindatecross,enddatecross,begin_date,"begin")
         
         //y-m-d
-        begin_date  = (today.getMonth()<10) ? today.getFullYear()+"-"+"0"+(enddatecross == 0&begindatecross == 0?today.getMonth()+1:today.getMonth())+"-"+begin_date : today.getFullYear()+"-"+(today.getMonth()+1)+"-"+begin_date;
-        end_date = (today.getMonth()<10) ? today.getFullYear()+"-"+"0"+(enddatecross == 0?today.getMonth()+1:today.getMonth())+"-"+end_date : today.getFullYear()+"-"+(today.getMonth()+1)+"-"+end_date;
+        begin_date  = (today.getMonth()<10) ? today.getFullYear()+"-"+"0"+(enddatecross === 0&begindatecross == 0?today.getMonth()+1:today.getMonth())+"-"+begin_date : today.getFullYear()+"-"+(today.getMonth()+1)+"-"+begin_date;
+        end_date = (today.getMonth()<10) ? today.getFullYear()+"-"+"0"+(enddatecross === 0?today.getMonth()+1:today.getMonth())+"-"+end_date : today.getFullYear()+"-"+(today.getMonth()+1)+"-"+end_date;
         
         return {
             begin:begin_date,
