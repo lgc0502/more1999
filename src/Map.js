f9a12f6b43abd42269c061412dfdadab1c6c2a77
YoChenYin <shelly7526@gmail.com>
2018-08-06 17:50:41 +0800

    set map zoom to min

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import L from 'leaflet'
+import ZoomMin from 'leaflet-zoom-min'
 import {feature} from "topojson-client"
 
 const a = {
@@ -101,24 +102,24 @@ class Map extends Component{
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
                     attribution:"&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" ,                    maxZoom:18,
                 }).addTo(map);    
-        map.addControl(new L.Control.ZoomMin())
-        var legend = L.control({position: 'bottomright'});
-        legend.onAdd = function (map) {
+        map.addControl(new ZoomMin())
+        // var legend = L.control({position: 'bottomright'});
+        // legend.onAdd = function (map) {
 
-            var div = L.DomUtil.create('div', 'info legend'),
-                grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-                labels = [];
+        //     var div = L.DomUtil.create('div', 'info legend'),
+        //         grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        //         labels = [];
 
-            // loop through our density intervals and generate a label with a colored square for each interval
-            for (var i = 0; i < grades.length; i++) {
-                div.innerHTML +=
-                    '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-                    grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-            }
+        //     // loop through our density intervals and generate a label with a colored square for each interval
+        //     for (var i = 0; i < grades.length; i++) {
+        //         div.innerHTML +=
+        //             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+        //             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        //     }
 
-            return div;
-        };
-        legend.addTo(map);
+        //     return div;
+        // };
+        // legend.addTo(map);
     }
    
     render(){
