3a1134655bc045db9b5daca93ea29d1fe9365413
YoChenYin <shelly7526@gmail.com>
2018-08-11 20:45:03 +0800

    test leaflet map data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
+import {BrowserRouter as Router,Route,PropsRoute,Link} from 'react-router-dom'
 import MainMenu from './MainMenu'
 import Historicalstatistics from './Historicalstatistics'
 import Instantnotification from './Instantnotification'
