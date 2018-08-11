6fb4d3ddebf5d291a26a5278f7aad30cfd348fcc
YoChenYin <shelly7526@gmail.com>
2018-08-11 20:52:14 +0800

    test leaflet map data

--- src/Historicalstatistics.jsx
+++ src/Historicalstatistics.jsx
@@ -40,6 +40,7 @@ class Historicalstatistics extends Component {
   }
   render() {
     const {isLoading,request_data} = this.state
+    console.log("in his")
     console.log(this.props.route.towngeo)
    if(isLoading){
      return (
