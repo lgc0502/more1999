a7d4960f31de787e9bf4aaaccfc07cc8205dd6e6
YoChenYin <shelly7526@gmail.com>
2018-08-12 02:11:59 +0800

    route test

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,11 +3,11 @@ import {Switch,Route,PropsRoute} from 'react-router-dom'
 import Historicalstatistics from './Historicalstatistics'
 import Instantnotification from './Instantnotification'
 const Main = (props) =>{
-   
+    return(
         <Switch>
             <PropsRoute path='/' component={Historicalstatistics} data={props}/>
             <Route path='/instantnotification' component={Instantnotification}/>
         </Switch>
-    
+    )
 }
 export default Main
