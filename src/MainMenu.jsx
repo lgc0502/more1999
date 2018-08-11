e86d29dbdaa23a5bccdfaa7fd8bd7f7e98f1b325
YoChenYin <shelly7526@gmail.com>
2018-08-11 19:38:31 +0800

    add return in MainMenu

--- src/MainMenu.jsx
+++ src/MainMenu.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {BrowserRouter as Link} from 'react-router-dom'
 
 const MainMenu = () =>{
-    <div>
-        <Link to="/">
-            {"上週累積"}
-        </Link>
-        <Link to="instantnotification">
-            {"待處理通報"}
-        </Link>
-    </div>
+    return(
+        <div>
+            <Link to="/">
+                <button>上週累積</button>
+            </Link>
+            <Link to="instantnotification">
+                <button>待處理通報</button>
+            </Link>
+        </div>
+    )
 }
 export default MainMenu
