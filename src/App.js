0b6872ec9622ce9682067a01e15f8e65900c4178
YoChenYin <shelly7526@gmail.com>
2018-08-12 20:57:14 +0800

    add file format in import place

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Header from './Header.jsx';
 //import Main from './Main.jsx';
 
-
 class App extends Component {
   constructor(props){
     super(props)
