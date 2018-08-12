9c78e3a62cf9a31cc74faad015a5ddb83e076a14
YoChenYin <shelly7526@gmail.com>
2018-08-13 00:51:43 +0800

    fix link

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const Header = () =>{
+const Header = ()=>{
     return(
-        <header className="App-header">
-            <Link to="/">上週累積</Link>
+        <header>
+            <Link to='/'>上週累積</Link>
             <Link to='instantnotification'>待處理通報</Link>
         </header>
     )
