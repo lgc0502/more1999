f9614c8346d3b88dffe7355c529498ca0625cf4b
YoChenYin <shelly7526@gmail.com>
2018-08-12 19:11:20 +0800

    change page from jsx to js

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,6 +3,7 @@ import {Switch,Route,PropsRoute} from 'react-router-dom'
 import Historicalstatistics from './Historicalstatistics'
 import Instantnotification from './Instantnotification'
 const Main = (props) =>{
+    
     return(
         <Switch>
             <PropsRoute exact path='/' component={Historicalstatistics} data={props}/>
