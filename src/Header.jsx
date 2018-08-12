934ac3fc5c847db81b00fb0d475ccd9034143777
YoChenYin <shelly7526@gmail.com>
2018-08-12 20:26:25 +0800

    add file format in import place

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 
-const Header = () =>{
-    return(
-        <header className="App-header">
-            <Link to="/"><text>上週累積</text></Link>
-            <Link to="/instantnotification"><text>待處理通報</text></Link>
-        </header>
-    )
-}
+// const Header = () =>{
+//     return(
+//         <header className="App-header">
+//             <Link to="/"><text>上週累積</text></Link>
+//             <Link to="/instantnotification"><text>待處理通報</text></Link>
+//         </header>
+//     )
+// }
+const Header =<header className="App-header">
+                <Link to="/"><text>上週累積</text></Link>
+                <Link to="/instantnotification"><text>待處理通報</text></Link>
+            </header>
 export default Header
--- src/Main.jsx
+++ src/Main.jsx
@@ -3,12 +3,18 @@ import {Switch,Route,PropsRoute} from 'react-router-dom'
 import Historicalstatistics from './Historicalstatistics.js'
 import Instantnotification from './Instantnotification.js'
 
-const Main=(props)=>{
-    return(
+// const Main=(props)=>{
+//     return(
+//         <Switch>
+//             <PropsRoute exact path='/' component={Historicalstatistics} data={props}/>
+//             <Route exact path='/instantnotification' component={Instantnotification}/>
+//         </Switch>
+//     )
+// }
+const Main=
         <Switch>
             <PropsRoute exact path='/' component={Historicalstatistics} data={props}/>
             <Route exact path='/instantnotification' component={Instantnotification}/>
         </Switch>
-    )
-}
+   
 export default Main
