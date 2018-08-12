3bd8eadda085c2b6e511f094bbf49c27a5813723
YoChenYin <shelly7526@gmail.com>
2018-08-12 23:42:28 +0800

    fix link

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ const Header = () =>{
     return(
         <header className="App-header">
             <Link to="/">上週累積</Link>
-            <Link to="/instantnotification">待處理通報</Link>
+            <Link to='instantnotification'>待處理通報</Link>
         </header>
     )
 }
