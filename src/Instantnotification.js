5812701bc57ca4ff8811ad6190e693566935c146
YoChenYin <shelly7526@gmail.com>
2018-08-17 03:46:54 +0800

    create dashboard

--- src/Instantnotification.js
+++ src/Instantnotification.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Listgroup from "./Listgroup.js";
 import Dropdown from './Dropdownsearch.js';
 import postApi from './postApi.js';
-
+import {RadialChart} from "react-vis";
 class Instantnotification extends Component{
     
     constructor(props){
@@ -34,8 +34,26 @@ class Instantnotification extends Component{
         }
         return(
             <div>
-                <div className="dashboard">
-
+                <div className="ui segment dashboard">
+                  <RadialChart  
+                      key={`dashboardRadial-${i}`}
+                      width={window.innerWidth*0.6}
+                      height={window.innerWidth*0.6}
+                      innerRadius={35}
+                      radius={40}
+                      getAngle={d => d}
+                      data={[89,11]}
+                      colorType="category"
+                      colorRange={["#9e9e9e69","#16982B"]} 
+                      stroke={null} 
+                      className="ui container radial-chart"  
+                  >
+                  </RadialChart>
+                  <h3>本週截至目前</h3>
+                  <h2>453 / 57</h2>
+                  <h3>處理 / 未處理</h3>
+                </div>
+                <div className="ui segment dashboard">
                 </div>
                 <Listgroup {...request_data}/> 
             </div>
