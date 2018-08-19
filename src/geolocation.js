912327e2376e68624165a3449e33aaa2e864cfca
YoChenYin <shelly7526@gmail.com>
2018-08-20 02:24:31 +0800

    send location

--- src/geolocation.js
+++ src/geolocation.js
@@ -1,20 +1,18 @@
 
 
-
+var p=new Object();
 export default {
     getLocation:()=>{
         if(navigator.geolocation){
-            console.log(navigator.geolocation.getCurrentPosition(showPosition));
+            navigator.geolocation.getCurrentPosition(showPosition);
         }else{
             alert("Geolocation is not supported by this brower.");
         }
+        return p;
     }
     
 }
 function showPosition(position){
-    var p = new Object();
     p['lat']=position.coords.latitude;
     p['lon']=position.coords.longitude;
-    console.log(p)
-    return p
 }
