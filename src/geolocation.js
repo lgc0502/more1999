6f9924a6414681f04da2064d1260e9c6fbe4478c
YoChenYin <shelly7526@gmail.com>
2018-08-20 00:50:17 +0800

    fix geolocation

--- src/geolocation.js
+++ src/geolocation.js
@@ -1,19 +1,20 @@
 
+
+
+export default {
+    getLocation:()=>{
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(showPosition(position));
+        }else{
+            alert("Geolocation is not supported by this brower.");
+        }
+    }
+    
+}
 function showPosition(position){
     var position = new Object();
     position['lat']=position.coords.latitude;
     position['lon']=position.coords.longitude;
     console.log(position)
     return position
-}
-
-export default {
-    getLocation:()=>{
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(showPosition);
-        }else{
-            alert("Geolocation is not supported by this brower.");
-        }
-    }
-    
 }
