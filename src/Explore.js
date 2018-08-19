785227877dee6b7224e1eb413e634637b18029e1
YoChenYin <shelly7526@gmail.com>
2018-08-20 01:32:41 +0800

    send location

--- src/Explore.js
+++ src/Explore.js
@@ -1,12 +1,13 @@
 import React ,{Component} from 'react';
 import Exploremap from './Exploremap.js';
-
+import geolocation from './geolocation'
 class Explore extends Component {
     constructor(props){
         super(props);
         this.state = {
             location:'',
-            lat_lng:''
+            lat_lng:'',
+            isloading:true
         };
     }
     updateInputValue(evt){
@@ -29,8 +30,22 @@ class Explore extends Component {
         //     })
         //  })
     }
+    componentDidMount(){
+        this.setState({
+            lat_lng:[geolocation.getLocation().lat,geolocation.getLocation().lon],
+            isloading:false
+        })
+
+    }
     render(){
-        console.log(this.props)
+        console.log(this.state.lat_lng)
+        if(this.state.isLoading){
+            return (
+             <div className="loaddata">
+               <h3 id="load_text">還在找 ......</h3>
+             </div>
+            )
+        }
         return (
             <div>
                 <h3>搜尋地點 查看通報狀況</h3>
--- src/Main.jsx
+++ src/Main.jsx
@@ -3,7 +3,7 @@ import {Switch,Route} from 'react-router-dom'
 import Historicalstatistics from './Historicalstatistics'
 import Instantnotification from './Instantnotification'
 import Explore from './Explore'
-import geolocation from './geolocation'
+
 const Main=(props)=>{ 
     const data = props
     return(
@@ -11,7 +11,7 @@ const Main=(props)=>{
             <Switch>
                 <Route exact path="/" component={Instantnotification}/>
                 <Route path="/history" render={()=>(<Historicalstatistics towngeo={data}/>)}/>
-                <Route path="/explore" render={()=>(<Explore position={geolocation.getLocation()} towngeo={data}/>)}/>
+                <Route path="/explore" render={()=>(<Explore  towngeo={data}/>)}/>
             </Switch>   
         </main>
     )
