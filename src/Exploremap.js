121324a7ed193825ab9e3e8522db63ed2fce2832
YoChenYin <shelly7526@gmail.com>
2018-08-22 16:27:15 +0800

    areachart

--- src/Exploremap.js
+++ src/Exploremap.js
@@ -2,8 +2,8 @@ import React,{Component} from 'react'
 import {Map,TileLayer,GeoJSON,Marker,Circle,CircleMarker,LayerGroup} from 'react-leaflet'
 import icon from 'icon.js';
                                            
-const typecolor = {"違規停車":'#2e1f54',"路燈故障":'#f00a36',"噪音舉發":'#ed3b21',"騎樓舉發":'#ff6908',"道路維修":'#ffc719',"交通運輸":'#598c14',"髒亂污染":'#335238',"民生管線":'#4a8594' ,"動物救援":'#706357'}
-const icon = {"違規停車":'parking-circle-slash',"路燈故障":'parking-circle-slash',"噪音舉發":'parking-circle-slash',"騎樓舉發":'parking-circle-slash',"道路維修":'parking-circle-slash',"交通運輸":'parking-circle-slash',"髒亂污染":'parking-circle-slash',"民生管線":'parking-circle-slash' ,"動物救援":'parking-circle-slash'}
+const typecolor = {"違規停車":'#2e1f54',"路燈故障":'#f00a36',"噪音舉發":'#ed3b21',"騎樓舉發":'#ff6908',"道路維修":'#ffc719',"交通運輸":'#598c14',"髒亂污染":'#335238',"民生管線":'#4a8594' ,"動物救援":'#706357'};
+const icon = {"違規停車":'parking-circle-slash',"路燈故障":'parking-circle-slash',"噪音舉發":'parking-circle-slash',"騎樓舉發":'parking-circle-slash',"道路維修":'parking-circle-slash',"交通運輸":'parking-circle-slash',"髒亂污染":'parking-circle-slash',"民生管線":'parking-circle-slash' ,"動物救援":'parking-circle-slash'};
 
 class Exploremap extends Component{
     constructor(props){
