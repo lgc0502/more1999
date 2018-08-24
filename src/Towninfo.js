21d5b9c37d41f3dab980659b7c90793ec980de93
YoChenYin <shelly7526@gmail.com>
2018-08-25 00:16:51 +0800

    modify history typing

--- src/Towninfo.js
+++ src/Towninfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {XYPlot,YAxis,HorizontalBarSeries} from "react-vis"
 const type = {"parking":'違規停車',"light":'路燈故障',"noise":'噪音舉發',"aisle":'騎樓舉發',"road":'道路維修',"traffic":'交通運輸',"dirty":'髒亂污染',"pipe":'民生管線',"animal":'動物救援'}
+const palette = ['red','orange','yellow','olive','green','teal','blue','violet','purple']
 export default class Towninfo extends Component {
     constructor(props){
         super(props)
@@ -19,21 +20,20 @@ export default class Towninfo extends Component {
         var responsetime = new Object;
         Object.keys(this.props.time).map((d)=>{
             let time = this.props.time[d].split(":")
-            if(time[0]===0)
+            if(time[0]==='0')
                 responsetime[d]=time[1]+'時'+time[2]+'分'
             else
                 responsetime[d]=[0]+'天'+time[1]+'時'+time[2]+'分' 
         })
-       console.log(responsetime)
         return(
             <div className="ui segment" style={{height:500}} >
                 <h3>{this.props.town}</h3>
                 <div>
-                {Object.keys(this.props.category).map((key)=>(
-                    <div className="ui segment history-mapinfo">
-                    <p><span style={{color:"#598c14"}}>{type[key]}</span></p>
-                      <p><span style={{color:"#598c14"}}>{responsetime[key]}</span></p>
-                      <p><span style={{color:"#598c14"}}>{this.props.category[key]}</span></p>
+                {Object.keys(this.props.category).map((key,i)=>(
+                    <div className={`ui ${palette[i]} segment history-mapinfo`}>
+                    <p><span style={{color:(this.props.category[key]==='0')?"#b0a696":"#000"}}>{type[key]}</span></p>
+                      <p><span style={{color:(this.props.category[key]==='0')?"#b0a696":"#000"}}>平均處理時間{responsetime[key]}</span></p>
+                      <p><span style={{color:(this.props.category[key]==='0')?"#b0a696":"#000"}}>通報數{this.props.category[key]}</span></p>
                     </div>     
                 ))}
                 </div>
