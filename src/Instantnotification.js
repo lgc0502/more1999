0f09601b01a52ccbda050c453b9264b48ba2345c
YoChenYin <shelly7526@gmail.com>
2018-08-30 14:34:50 +0800

    modify typing instant part

--- src/Instantnotification.js
+++ src/Instantnotification.js
@@ -1,24 +1,48 @@
 import React, { Component } from "react";
+import ReactDOM from 'react-dom';
 import postApi from './postApi.js';
 import {XYPlot,RadialChart,HorizontalBarSeries,YAxis} from "react-vis";
 import Listgroup from "./Listgroup.js";
 
-const palette = ['#2e1f54','#f00a36','#ed3b21','#ff6908','#ffc719','#598c14','#335238','#4a8594','#706357']
-
 const transtype = {"animal":'動物救援',"pipe":'民生管線',"dirty":'髒亂污染',"traffic":'交通運輸',"road":'道路維修',"aisle":'騎樓舉發',"noise":'噪音舉發',"light":'路燈故障',"parking":'違規停車'}
+
 class Instantnotification extends Component{
     
   constructor(props){
     super(props)
     this.state = {
+      width:props.width||-1,
+      height:props.height||-1,
       isLoading: true,
       request_data: {},
       finish_bar_data:{},
       unfinish_bar_data:{}
     }
   }
-
+  updateSize(){
+    try{
+      const parentDom = ReactDOM.findDOMNode(this).parentNode;
+      let {width,height} = this.props;
+      
+      if(!width){
+        width = parentDom.offsetWidth;
+        
+        if(width>600){
+          width = width*0.35
+          height = width*0.8
+        }else{
+          width = width*0.7
+          height = width*0.75
+        }
+      }
+      
+      this.setState({width,height});
+    }catch(ignore){}
+  }
   componentDidMount(){
+    this.updateSize();
+    window.addEventListener('resize',this.updateSize.bind(this));
+    
     postApi.requertPost('./this_week_data','null').then(data => {
         
       this.setState({
@@ -33,12 +57,12 @@ class Instantnotification extends Component{
       })
     })
   }
-
+  componentWillUnmount(){
+    window.removeEventListener('resize',this.updateSize.bind(this));
+  }
   render(){
     const {isLoading,request_data,finish_bar_data,unfinish_bar_data} = this.state
-    console.log(request_data)
-    console.log(finish_bar_data)
-    console.log(unfinish_bar_data)
+    
     if(isLoading){
       return (
         <div class="loaddata">
@@ -48,52 +72,53 @@ class Instantnotification extends Component{
     }
     return(
       <div>
-        <div className="board">
+        <div className="instant board">
           <div className="ui segment dashboard">
             <svg className="hint">
-              <circle  style={{cx:"15",cy:"15",r:"5",fill:"#598c14"}}/></svg> 
+              <circle  style={{cx:"50%",cy:"50%",r:"5",fill:"#598c14"}}/></svg> 
             <span className="hint">已處理</span>
             <svg className="hint">
-              <circle  style={{cx:"15",cy:"15",r:"5",fill:"#b0a696",}}/></svg> 
+              <circle  style={{cx:"50%",cy:"50%",r:"5",fill:"#b0a696",}}/></svg> 
             <span className="hint">處理中</span>
             <RadialChart  
               key={`dashboardRadial`}
-              width={window.innerWidth*0.1}
-              height={window.innerWidth*0.1}
-              innerRadius={35}
-              radius={45}
+              width={window.innerWidth*0.18}
+              height={window.innerWidth*0.18}
+              innerRadius={window.innerWidth*0.05-window.innerWidth*0.01}
+              radius={window.innerWidth*0.05}
               getAngle={d => d}
               data={[request_data.res.FinishRate.finish[0],request_data.res.FinishRate.unfinish[0]]}
               colorType="category"
               colorRange={["#b0a696","#598c14"]} 
               stroke={null} 
               className="ui container radial-chart instantdonut">
-              <svg id="donutratio" width="90" height="90">
-                <circle  style={{cx:"40",cy:"40",r:"32",fill:"#b0a696",opacity:0.4 }}/></svg> 
+              {/* <svg id="donutratio" width="90" height="90">
+                <circle  style={{cx:"40%",cy:"40%",r:"32",fill:"#b0a696",opacity:0.4 }}/></svg>  */}
+              <span className="ratio">完成度</span>
               <span className="ratio">{((request_data.res.FinishRate.finish[0]/(request_data.res.FinishRate.finish[0]+request_data.res.FinishRate.unfinish[0]))*100).toFixed(1)+"%"}</span>                  
-              <span className="ratio">complete</span>
+              
             </RadialChart>
            
             <h3>本週截至目前通報</h3>
-            <h2>{request_data.res.FinishRate.finish[0]} / {request_data.res.FinishRate.unfinish[0]}</h2>
-            <h3 id="ratiohint" style={{fontSize:12}}>處理 / 未處理</h3>
+            <h3><font color="#598c14">{request_data.res.FinishRate.finish[0]}</font> / <font color="#b0a696">{request_data.res.FinishRate.unfinish[0]}</font></h3>
+            <span id="ratiohint" style={{fontSize:12}}><font color="#598c14">處理</font> / <font color="#b0a696">未處理</font></span>
           </div>
           <div className="ui segment dashboard">
-            <div className="instant-descript-data">
+          <div width={this.state.width*0.2} height={this.state.height} className="instant-descript-data">
               {Object.keys(request_data.res.Category).map((key)=>(
                   <p><span style={{color:"#598c14"}}>{request_data.res.Category[key][0]}</span> / <span style={{color:"#b0a696"}}>{request_data.res.Category[key][1]}</span></p>))}
             </div>
             <XYPlot
-              width={window.innerWidth*0.32}
-              height={window.innerWidth*0.25}
+              width={this.state.width}
+              height={this.state.height}
               yType='ordinal'
-              xRange={[95,window.innerWidth*0.3]}
-              yRange={[0,window.innerWidth*0.23]}
+              xRange={[this.state.width*0.2,this.state.width]}
+              yRange={[0,this.state.height]}
               stackBy='x'
               className="instant-category-chart">
               <YAxis
                 className="categoryaxe"
-                width={window.innerWidth*0.06}
+                width={this.state.width*0.2}
                 style={{strokeOpacity:0}}
                 top={1}/>
               <HorizontalBarSeries
