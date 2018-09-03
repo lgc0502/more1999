416d4155a5bb1375842dd7aecbc329c6796d54ae
YoChenYin <shelly7526@gmail.com>
2018-09-04 02:10:24 +0800

    legend

--- src/Personalreport.js
+++ src/Personalreport.js
@@ -115,9 +115,10 @@ class Personalreport extends Component {
         return (
             <div className="Personalreport">
                 
-                <h4>{this.state.address}</h4>
+                
                 <h2>ON AIR</h2>
                 <svg className="line-block"><line x1="40%" y1="0" x2="60%" y2="0" stroke="gray"/></svg>
+                <h4>{this.state.address}</h4>
                 <div className="ui input">
                     <input type="text" placeholder="搜尋地點 查看通報狀況..." value={this.state.location} onChange={evt=>this.updateInputValue(evt)}/>
                     <button className="ui small icon button" onClick={this.handleclick.bind(this)}>
